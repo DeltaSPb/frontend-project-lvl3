dfcf7b326c940647e01d1ffbe81b578a228b7163
DeltaSPb <oipts3012@gmail.com>
2019-09-30 12:42:00 +0300

    fix some problems

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index_bandle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node-modules/',
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
+  devtool: process.env.NODE_ENV ? '' : 'source-map',
+};

916488ffc1c0d0abb6e1ab4640402e40daa90e4a
DeltaSPb <oipts3012@gmail.com>
2019-11-25 11:38:37 +0300

    fix - common naming with RSS structure

--- src/parser.js
+++ src/parser.js
@@ -2,17 +2,12 @@ export default (data) => {
   const parser = new DOMParser();
   const html = parser.parseFromString(data, 'application/xml');
 
-  const information = {
-    title: html.querySelector('title').textContent,
-    description: html.querySelector('description').textContent,
-    link: html.querySelector('link').textContent,
-  };
-
-  const list = [...html.querySelectorAll('item')].map((item) => ({
+  const title = html.querySelector('title').textContent;
+  const description = html.querySelector('description').textContent;
+  const posts = [...html.querySelectorAll('item')].map((item) => ({
     title: item.querySelector('title').textContent,
     description: item.querySelector('description').textContent,
     link: item.querySelector('link').textContent,
   }));
-
-  return { information, list };
+  return { title, description, posts };
 };
